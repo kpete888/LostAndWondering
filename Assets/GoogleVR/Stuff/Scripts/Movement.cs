105e39e9c4f16b6c058a54f7e008e07871eecfc6
Memo Khoury <majd.khoury93@gmail.com>
2016-10-02 20:51:12 -0400

    First Scene

---
+++ Assets/GoogleVR/Stuff/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public float playerSpeed = 2.0f;
+    public bool didBumpIntoCube = false;
+
+    public GameObject Light1;
+    public GameObject Light2;
+    public GameObject Light3; 
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed);
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        didBumpIntoCube = true; 
+
+        if (didBumpIntoCube)
+        {
+            Light1.SetActive(true);
+            Light2.SetActive(true);
+            Light3.SetActive(true);
+        }
+            
+    }
+}
